49ac7436a226069735732c0684461324065ba398
shubhamsaini123 <shubham.sainipydev@gmail.com>
2022-07-19 17:45:38 +0530

    changes

--- api/controllers/billdesk_controller.go
+++ api/controllers/billdesk_controller.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"time"
 
 	jose "github.com/dvsekhvalnov/jose2go"
 	"github.com/gin-gonic/gin"
@@ -44,14 +46,23 @@ func BillDeskJwtTokenGenerate() (map[string]interface{}, error) {
 		err error
 		// token string
 	)
+	amount := (rand.Float64() * 8) + 7
+
 	var obj map[string]interface{}
-	str := []byte(`{"mercid":"FITPAS2UAT","orderid":"DSCGD113112F","amount":"320.00","order_date":"2022-07-19T17:24:00+0530","currency":"320","ru":"https://www.example.com/merchant/api/pgresponse","additional_info":{"additional_info1":"Details1","additional_info2":"Details2"},"itemcode":"DIRECT","device":{"init_channel":"internet","user_agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0","accept_header":"text/html"}}`)
+	n := 10
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		panic(err)
+	}
+	orderid := fmt.Sprintf("%X", b)
+	str := []byte(fmt.Sprintf(`{"mercid":"FITPAS2UAT","orderid":"%s","amount":"%v","order_date":"%s","currency":%v,"ru":"https://www.example.com/merchant/api/pgresponse","additional_info":{"additional_info1":"Details2"},"itemcode":"DIRECT","device":{"init_channel":"internet","user_agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0","accept_header":"text/html"}}`, orderid, amount, time.Now().Format("2006-01-02 15:04:05"), amount))
 	json.Unmarshal(str, &obj)
 	obj["ip"] = GetOutboundIP()
 
 	bytes, err := json.Marshal(obj)
-
-	token, err := jose.Sign(string(bytes), jose.HS256, []byte("KEHpqq5UWQFwHnL6OBvMr7mln6OWWP3k"),
+	fmt.Println(err)
+	var token string
+	token, err = jose.Sign(string(bytes), jose.HS256, []byte("KEHpqq5UWQFwHnL6OBvMr7mln6OWWP3k"),
 		jose.Header("clientid", "fitpas2uat"),
 		jose.Header("alg", "HS256"), jose.Header("kid", "HMAC"))
 	obj["token"] = token
